862d3ea998748a9ced1784f783b1ea3e06747b4a
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2019-11-13 19:35:13 +0000

    Create save method

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -8,23 +8,30 @@ export class UserForm extends View<User, UserProps> {
 
   template(): string {
     return `
-      <div>User name: ${this.model.get('name')}</div>
-      <div>User age: ${this.model.get('age')}</div>
-      <input type="text" class="input-name" />
+      <input 
+        type="text" 
+        class="input-name"
+        placeholder="${this.model.get('name')}" />
       <button class="change-name">Change Name</button>
       <button class="set-random-age">Set random age</button>
+      <button class="save-model">Save</button>
     `;
   }
 
   eventsMap(): EventMap {
     return {
       'click:.set-random-age': this.onSetAgeClick,
-      'click:.change-name': this.onChangeName
+      'click:.change-name': this.onChangeName,
+      'click:.save-model': this.onSave
     };
   }
 
+  onSave = (): void => {
+    this.model.save();
+  }
+
   onChangeName = (): void => {
-    const input = this.parent.querySelector('input');
+    const input = this.parent.querySelector('.input-name') as HTMLInputElement;
     if (input) {
       this.model.set({ name: input.value });
     }
